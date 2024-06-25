75fff24a36b07712508f7feb0617ad89bb45fa65
FouadAllaoui <fouadallaoui70@gmail.com>
2024-06-25 21:16:52 +0100

    use foreach maping instances

--- main.tf
+++ main.tf
@@ -93,6 +93,26 @@ resource "google_compute_instance" "web-instances" {
   }
 }
 
+## WEBSERVERS-MAP
+resource "google_compute_instance" "web-map-instances" {
+  for_each = var.environment_instance_settings
+  name         = "${lower(each.key)}-web"
+  machine_type = each.value.machine_type
+  labels = each.value.labels
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    # A default network is created for all GCP projects
+    network = data.google_compute_network.default.self_link
+    subnetwork = google_compute_subnetwork.subnet-1.self_link
+  }
+
+}
+
 ## DB
 resource "google_compute_instance" "mysqldb" {
   name         = "mysqldb"
