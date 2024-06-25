800d5a2e763626f0200510b948377d8336b0445e
FouadAllaoui <fouadallaoui70@gmail.com>
2024-06-25 21:11:38 +0100

    update vars and use foreach to create buckets

--- main.tf
+++ main.tf
@@ -36,6 +36,16 @@ resource "google_compute_firewall" "default" {
   source_tags = var.compute-source-tags
 }
 
+## BUCKETS
+resource "google_storage_bucket" "environment_buckets" {
+  for_each = toset(var.environment_list)
+  name = "${lower(each.key)}_${var.project-id}"
+  location = "US"
+  versioning {
+    enabled = true
+  }
+}
+
 ### COMPUTE
 ## NGINX PROXY
 resource "google_compute_instance" "nginx_instance" {
@@ -61,7 +71,7 @@ resource "google_compute_instance" "nginx_instance" {
   }
 }
 
-## WEB-instances
+## WEBSERVERS
 resource "google_compute_instance" "web-instances" {
   count = 3
   name         = "web${count.index}"
@@ -82,66 +92,6 @@ resource "google_compute_instance" "web-instances" {
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }
 }
-/*
-## WEB1
-resource "google_compute_instance" "web1" {
-  name         = "web1"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    envirenment = var.environment_map[var.target_environment]
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB2
-resource "google_compute_instance" "web2" {
-  name         = "web2"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    envirenment = var.environment_map[var.target_environment]
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB3
-resource "google_compute_instance" "web3" {
-  name         = "web3"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    envirenment = var.environment_map[var.target_environment]
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
-}
-*/
 
 ## DB
 resource "google_compute_instance" "mysqldb" {
@@ -162,30 +112,4 @@ resource "google_compute_instance" "mysqldb" {
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }  
 }
-
-
-resource "random_id" "db_name_suffix" {
-  byte_length = 4
-}
-
-## CLOUD SQL
-resource "google_sql_database_instance" "cloudsql" {
-  name             = "web-app-db-${random_id.db_name_suffix.hex}"
-  database_version = "MYSQL_8_0"
-  region           = "us-central1"
-
-  settings {
-    tier = "db-e2-micro"
-  }
-  deletion_protection = false
-}
-
-## CLOUD SQL USER
-resource "google_sql_user" "users" {
-  name     = var.dbusername
-  instance = google_sql_database_instance.cloudsql.name
-  password = "notsecurepassword"
-}
-
-
 
